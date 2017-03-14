2ae4df52d323838e624a7413366873489a1d5464
Chris Coralli <ccoralli@gmail.com>
2017-03-14 14:28:53 -0400

    deployed to heroku.

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,11 @@ const ContactController = require('../controllers/contact')
 
 
 module.exports = (app) => {
+  app.use(function(req, res, next) {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      next();
+  });
   app.post('/contact', ContactController.create);
   app.get('/contact', ContactController.listContacts);
   app.get('/contact/:id', ContactController.findContact);
